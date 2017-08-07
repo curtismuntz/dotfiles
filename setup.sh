4a8db9d0e6b4dceceb3a4c7cbe9c4a2f5b777baa
= <=>
2017-08-06 22:10:26 -0700

    big cleanup

--- setup.sh
+++ setup.sh
@@ -7,18 +7,29 @@ finish() {
  };
 trap finish EXIT INT TERM
 
+force_symlink_arg1TARGET_arg2SOURCE() {
+	if ~ [ -f $1 ]; then
+		ln -s $2 $1
+	else
+		rm $1
+		ln -s $1 $2
+	fi
+}
+
 echo "this will set up dot files to their symlinks"
-DIR=`pwd`
-if ! [ -f "$HOME/.bashrc" ]; then ln -s $DIR/bash/bashrc ~/.bashrc; fi
+#DIR=`pwd`
+DIR = $HOME/dotfiles
+force_symlink_arg1TARGET_arg2SOURCE $HOME/.bashrc $DIR/bash/bashrc
+force_symlink_arg1TARGET_arg2SOURCE $HOME/.zshrc $DIR/zsh/zshrc
+force_symlink_arg1TARGET_arg2SOURCE $HOME/.vimrc $DIR/vim/vimrc
+force_symlink_arg1TARGET_arg2SOURCE $HOME/.gitconfig $DIR/git/gitconfig
+force_symlink_arg1TARGET_arg2SOURCE $HOME/.gitconfig $DIR/git/gitconfig
+force_symlink_arg1TARGET_arg2SOURCE $HOME/.aliases $DIR/bash/aliases
+
 if ! [ -f "$HOME/.environment" ]; then ln -s $DIR/bash/environment ~/.environment; fi
-if ! [ -f "$HOME/.aliases" ]; then ln -s $DIR/bash/aliases ~/.aliases; fi
 if ! [ -f "$HOME/.devpaths" ]; then ln -s $DIR/bash/devpaths ~/.devpaths; fi
-if ! [ -f "$HOME/.vimrc" ]; then ln -s $DIR/vim/vimrc ~/.vimrc; fi
-if ! [ -f "$HOME/.zshrc" ]; then ln -s $DIR/zsh/zshrc ~/.zshrc; fi
-if ! [ -f "$HOME/.gitconfig" ]; then ln -s $DIR/git/gitconfig ~/.gitconfig; fi
 if ! [ -f "$HOME/.dockerfuncs" ]; then ln -s $DIR/docker/dockerfuncs ~/.dockerfuncs; fi
 if ! [ -f "$HOME/.bazelrc" ]; then ln -s $DIR/bazel/bazelrc ~/.bazelrc; fi
 
 # Make a directory for vim persistent undo
 mkdir -p $HOME/.undodir
-
