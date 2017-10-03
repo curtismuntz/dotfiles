df1c06e7a6795145b93ef5f846e3c0aa0ecceab0
murt <cmuntz@outlook.com>
2017-10-03 12:02:40 -0700

    setup script cleanup

--- setup.sh
+++ setup.sh
@@ -7,12 +7,12 @@ finish() {
  };
 trap finish EXIT INT TERM
 
-force_symlink_arg1TARGET_argLINKNAME() {
+force_ln_s() {
 	if ! [ -f $2 ]; then
 		ln -s $1 $2
 	else
-		echo "REMOVING OLD RC $1, LINKING $2"
-		rm $2
+		echo "BACKUP OLD RC $1, LINKING $2"
+		mv $2 $2_bkup
 		ln -s $1 $2
 	fi
 }
@@ -21,17 +21,16 @@ echo "this will set up dot files to their symlinks"
 #DIR=`pwd`
 DIR=$HOME/murt/dotfiles
 echo "using DIR=$DIR"
-force_symlink_arg1TARGET_argLINKNAME "$DIR/bash/bashrc" "$HOME/.bashrc"
-force_symlink_arg1TARGET_argLINKNAME "$DIR/zsh/zshrc" "$HOME/.zshrc"
-force_symlink_arg1TARGET_argLINKNAME "$DIR/vim/vimrc" "$HOME/.vimrc"
-force_symlink_arg1TARGET_argLINKNAME "$DIR/git/gitconfig" "$HOME/.gitconfig"
-force_symlink_arg1TARGET_argLINKNAME "$DIR/git/gitconfig" "$HOME/.gitconfig"
-force_symlink_arg1TARGET_argLINKNAME "$DIR/bash/aliases" "$HOME/.aliases"
+
+# These are commonly installed by packages. Force the symlink by backing up existing files first if
+# the exist
+force_ln_s "$DIR/bash/bashrc" "$HOME/.bashrc"
+force_ln_s "$DIR/zsh/zshrc" "$HOME/.zshrc"
+force_ln_s "$DIR/vim/vimrc" "$HOME/.vimrc"
+force_ln_s "$DIR/git/gitconfig" "$HOME/.gitconfig"
+force_ln_s "$DIR/bash/aliases" "$HOME/.aliases"
 
 if ! [ -f "$HOME/.environment" ]; then ln -s $DIR/bash/environment $HOME/.environment; fi
 if ! [ -f "$HOME/.devpaths" ]; then ln -s $DIR/bash/devpaths $HOME/.devpaths; fi
 if ! [ -f "$HOME/.dockerfuncs" ]; then ln -s $DIR/docker/dockerfuncs $HOME/.dockerfuncs; fi
 if ! [ -f "$HOME/.bazelrc" ]; then ln -s $DIR/bazel/bazelrc $HOME/.bazelrc; fi
-
-# Make a directory for vim persistent undo
-mkdir -p $HOME/.undodir
