9d6d20b136bb6ca69d3e6f1b40121e26c123d276
Curtis Muntz <cmuntz@a17.ai>
2019-02-09 22:40:43 +0000

    update setup.sh for crostini

--- setup.sh
+++ setup.sh
@@ -5,9 +5,20 @@ finish() {
 	 # do stuff
    # exit traps! holy shit!
 	echo "finishing..."
+};
 
- };
 trap finish EXIT INT TERM
+
+force_ln_s() {
+  # if the file doesn't exist, link it; otherwise backup the old file and link it
+	if ! [ -f $2 ]; then
+		ln -s $1 $2
+	else
+		echo "BACKUP OLD RC $1, LINKING $2"
+		mv $2 $2_bkup
+		ln -s $1 $2
+	fi
+}
 
 install_docker() {
 	sudo apt-get install -y \
@@ -48,53 +59,53 @@ install_deps() {
 # To install tilix sudo add-apt-repository ppa:webupd8team/terminix
 #		tilix
 
-	install_docker
-
 	sudo chsh --shell $(which zsh) $(whoami)
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+}
 
+symlink_dotfiles() {
+	echo "this will set up dot files to their symlinks"
+	#DIR=`pwd`
+	DIR=$HOME/murt/dotfiles
+	echo "using DIR=$DIR"
+
+	# These are commonly installed by packages. Force the symlink by backing up existing files first if
+	# the exist
+	force_ln_s "$DIR/bash/bashrc" "$HOME/.bashrc"
+	force_ln_s "$DIR/zsh/zshrc" "$HOME/.zshrc"
+	force_ln_s "$DIR/vim/vimrc" "$HOME/.vimrc"
+	force_ln_s "$DIR/git/gitconfig" "$HOME/.gitconfig"
+	force_ln_s "$DIR/bash/aliases" "$HOME/.aliases"
+
+	if ! [ -f "$HOME/.environment" ]; then ln -s $DIR/bash/environment $HOME/.environment; fi
+	if ! [ -f "$HOME/.devpaths" ]; then ln -s $DIR/bash/devpaths $HOME/.devpaths; fi
+	if ! [ -f "$HOME/.dockerfuncs" ]; then ln -s $DIR/docker/dockerfuncs $HOME/.dockerfuncs; fi
+	if ! [ -f "$HOME/.bazelrc" ]; then ln -s $DIR/bazel/bazelrc $HOME/.bazelrc; fi
+	if ! [ -f "$HOME/.tmux.conf" ]; then ln -s $DIR/tmux/tmux.conf $HOME/.tmux.conf; fi
+	if ! [ -f "$HOME/.pythonrc" ]; then ln -s $DIR/python/pythonrc $HOME/.pythonrc; fi
+}
+
+install_snaps() {
 	echo "Installing snap apps"
 	sudo snap install spotify
 	# sudo snap install slack --classic
 	sudo snap install discord
 	# sudo snap install atom --classic
-
-	echo "Install oh-my-zsh manually"
 }
 
-force_ln_s() {
-  # if the file doesn't exist, link it; otherwise backup the old file and link it
-	if ! [ -f $2 ]; then
-		ln -s $1 $2
-	else
-		echo "BACKUP OLD RC $1, LINKING $2"
-		mv $2 $2_bkup
-		ln -s $1 $2
-	fi
-}
 
-echo "this will set up dot files to their symlinks"
-#DIR=`pwd`
-DIR=$HOME/murt/dotfiles
-echo "using DIR=$DIR"
-
+## MAIN
 if [ $(id -u) = 0 ]; then
    echo "Do not run this as root"
    exit 1
 fi
 
+# Only install docker and snap apps if not on crostini
+if [[ $(hostname) != "penguin" ]]; then
+	install_docker
+	install_snaps
+	echo "not penguin"
+fi
+
 install_deps
-
-# These are commonly installed by packages. Force the symlink by backing up existing files first if
-# the exist
-force_ln_s "$DIR/bash/bashrc" "$HOME/.bashrc"
-force_ln_s "$DIR/zsh/zshrc" "$HOME/.zshrc"
-force_ln_s "$DIR/vim/vimrc" "$HOME/.vimrc"
-force_ln_s "$DIR/git/gitconfig" "$HOME/.gitconfig"
-force_ln_s "$DIR/bash/aliases" "$HOME/.aliases"
-
-if ! [ -f "$HOME/.environment" ]; then ln -s $DIR/bash/environment $HOME/.environment; fi
-if ! [ -f "$HOME/.devpaths" ]; then ln -s $DIR/bash/devpaths $HOME/.devpaths; fi
-if ! [ -f "$HOME/.dockerfuncs" ]; then ln -s $DIR/docker/dockerfuncs $HOME/.dockerfuncs; fi
-if ! [ -f "$HOME/.bazelrc" ]; then ln -s $DIR/bazel/bazelrc $HOME/.bazelrc; fi
-if ! [ -f "$HOME/.tmux.conf" ]; then ln -s $DIR/tmux/tmux.conf $HOME/.tmux.conf; fi
-if ! [ -f "$HOME/.pythonrc" ]; then ln -s $DIR/python/pythonrc $HOME/.pythonrc; fi
+symlink_dotfiles
