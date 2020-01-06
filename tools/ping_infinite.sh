e8fea2bbd07a3a82b3a948dcd99421a25112ccc0
Curtis Muntz <me@murt.is>
2020-01-06 09:47:58 -0800

    Add usage to ping_infinite

--- tools/ping_infinite.sh
+++ tools/ping_infinite.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+set -e
+
 server=$1
+if [[ -z ${server} ]]; then
+  echo "Usage: $0 <servername or ip>"
+  exit 1
+fi
+
+# https://askubuntu.com/a/929711/819866
 printf "%s" "waiting for $server..."
 while ! ping -c 1 -n -w 1 $server &> /dev/null
 do
